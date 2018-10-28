3693eb5b3ae94c6e4a4be2ab9324d0fe30e628dd
Mustafa Al Ibrahim <MosGeo@users.noreply.github.com>
2018-10-28 02:21:26 -0700

    Initial prototype

---
+++ Source/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+
+    float rotationHorizontalSpeed = 2.0f;
+    float rotationVerticalSpeed = 2.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float sideway = Input.GetAxis("Horizontal");
+        float forward = Input.GetAxis("Vertical");
+        float speed = 100;
+
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * forward * speed);
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.right * sideway * speed);
+        //gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, sideway* Time.deltaTime * speed,0);
+
+
+        // Mouse rotation
+        float h = rotationHorizontalSpeed * Input.GetAxis("Mouse X");
+        //gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, h, 0);
+
+        gameObject.GetComponent<Transform>().Rotate(0, h, 0);
+
+        float v = rotationHorizontalSpeed * Input.GetAxis("Mouse Y");
+        //Camera.main.GetComponent<Transform>().Rotate(v, 0, 0);
+
+
+    }
+}
---
+++ Source/Assets/Scripts/Scan.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scan : MonoBehaviour
+{
+
+    public GameObject AudioSources;
+    public float maxDistance = 10.0f;
+    bool autoScan = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("f") || autoScan == true)
+        {
+            startScan();
+        }
+    }
+
+    void startScan()
+    {
+        RaycastHit hit;
+
+        float[] anglesY = {0.0f, 22.5f, 45, -45, -22.5f };
+        float[] anglesX = {-1, -30};
+
+
+        for (int i = 0; i < anglesY.Length; i += 1)
+        {
+            Vector3 direction = createScanDirectionY(transform.TransformDirection(Vector3.forward), anglesY[i]);
+
+            for (int j = 0; j < anglesX.Length; j += 1)
+            {
+                direction = createScanDirectionX(direction, anglesX[j]);
+                if (Physics.Raycast(transform.position, direction, out hit, maxDistance))
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                    initializeAudioSource(hit);
+                }
+            }
+        }
+
+    }
+
+    void initializeAudioSource(RaycastHit hit)
+    {
+        string audioFileName = "Audio/WhiteNoise";
+        GameObject audioSourceGO = new GameObject();
+        audioSourceGO.transform.parent = AudioSources.transform;
+        AudioSource audioSource = audioSourceGO.AddComponent<AudioSource>();
+        audioSourceGO.transform.Translate(hit.point);
+        audioSource.clip = Resources.Load(audioFileName) as AudioClip;
+        float startTime = Random.Range(0.0f, audioSource.clip.length);
+        audioSource.time = startTime;
+        audioSource.volume = (maxDistance -  hit.distance)/maxDistance;
+        audioSource.volume = Mathf.Max(0, audioSource.volume);
+        audioSource.dopplerLevel = 0;
+        audioSource.spatialBlend = 1;
+        audioSource.Play();
+        Destroy(audioSourceGO, .1f);
+    }
+
+    Vector3 createScanDirectionY(Vector3 forward, float angle)
+    {
+        Vector3 vector = Quaternion.Euler(0, angle, 0) * forward;
+        return vector;
+    }
+
+    Vector3 createScanDirectionX(Vector3 forward, float angle)
+    {
+        Vector3 vector = Quaternion.Euler(angle, 0, 0) * forward;
+        return vector;
+    }
+
+}
